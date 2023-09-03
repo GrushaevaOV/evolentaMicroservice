5bed1e48a468fe1820dc3fbb23f5c9b538281d94
GrishaevaOV <olga.gr2812@mail.ru>
2023-09-03 16:17:41 +0300

    Location

---
+++ LocationEvolenta/src/main/java/com/example/locationEvolenta/LocationEvolentaApplication.java
@@ -0,0 +1,13 @@
+package com.example.locationEvolenta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationEvolentaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocationEvolentaApplication.class, args);
+    }
+
+}
